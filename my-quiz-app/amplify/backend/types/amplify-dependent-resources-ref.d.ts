cbb2e42645a22873273fc29890a00c68b0769bbe
Jesus Gomez <2708jesusgz@gmail.com>
2025-02-25 19:13:06 +0000

    Add authentication with Cognito

--- my-quiz-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ my-quiz-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myquizapp63516647": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
